371eb6dc401e22e898bed80614b5ac0da1fa0a63
kinich-web <ivanterzi96@gmail.com>
2024-12-04 20:08:24 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace практикум_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1
+            //Console.Write("a = ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b = ");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"a + b = {a+b}");
+            //Console.ReadKey();
+            //Task 2
+            //Console.Write("side1 = ");
+            //int side1 = int.Parse(Console.ReadLine());
+            //Console.Write("side2 = ");
+            //int side2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"S = {a * b}");
+            //Console.WriteLine($"P = {(a + b) * 2}");
+            //Console.ReadKey();
+            //Task 3
+            //Console.Write("leg1 = ");
+            //int leg1 = int.Parse(Console.ReadLine());
+            //Console.Write("leg2 = ");
+            //int leg2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"hypotenuse = {Math.Sqrt((a*a)+(b*b))}");
+            //Console.ReadKey();
+            //Task 4
+            //Console.Write("side = ");
+            //int side = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"S = {side * side}");
+            //Console.WriteLine($"P = {(side + side)*2}");
+            //Console.ReadKey();
+
+
+        }
+    }
+}
